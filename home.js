5cd11112da9c27057085ad633e46b65c0c4835e8
RileyRiceAndMilk <167301632+RileyRiceAndMilk@users.noreply.github.com>
2024-11-30 11:50:11 +0100

    Update home.js

--- home.js
+++ home.js
@@ -28,7 +28,7 @@ let debounceTimer;
 
 const createClearButton = (inputElement) => {
     const clearButton = document.createElement('span');
-    clearButton.classList.add('clear-input');
+    clearButton.classList.add('clear-input','clear-input-search');
     clearButton.textContent = '×';
     inputElement.parentElement.appendChild(clearButton);
     clearButton.style.display = 'none';
@@ -365,5 +365,4 @@ function filterRecipes() {
 
 initializeFilters(recipes);
 displayRecipes(recipes);
-
 
