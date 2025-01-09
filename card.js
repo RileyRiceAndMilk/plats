c18e745bd57004e8336035dfd76d44f88e3db377
RileyRiceAndMilk <167301632+RileyRiceAndMilk@users.noreply.github.com>
2025-01-09 11:38:56 +0100

    Add files via upload

---
+++ card.js
@@ -0,0 +1,87 @@
+export class RecipeCard {
+    constructor(recipe) {
+        this.recipe = recipe;
+    }
+
+    static createCard(recipe) {
+        const card = new RecipeCard(recipe);
+        return card.createCardContent();
+    }
+
+    createCardContent() {
+        if (!this.recipe) {
+            throw new Error("Aucune recette n'a été fournie !");
+        }
+
+        const card = document.createElement('article');
+        card.classList.add('recipe-card');
+        card.setAttribute('data-recipe-id', this.recipe.id);
+
+        const imageContainer = document.createElement('div');
+        imageContainer.classList.add('image-container');
+        const img = document.createElement('img');
+        img.src = `recipes/${this.recipe.image}`;
+        img.alt = `Image de ${this.recipe.name}`;
+        img.classList.add('recipe-image');
+        img.onerror = () => {
+            img.style.display = 'none';
+            const message = document.createElement('p');
+            message.textContent = "Désolé, l'image est indisponible.";
+            imageContainer.appendChild(message);
+        };
+
+        const timeTag = document.createElement('span');
+        timeTag.classList.add('time-tag');
+        timeTag.textContent = `${this.recipe.time} min`;
+
+        imageContainer.appendChild(img);
+        imageContainer.appendChild(timeTag);
+
+        const cardContentContainer = document.createElement('div');
+        cardContentContainer.classList.add('card-content-container');
+
+        const title = document.createElement('h2');
+        title.id = `recipe-${this.recipe.id}`;
+        title.textContent = this.recipe.name;
+
+        const recetteTitle = document.createElement('h3');
+        recetteTitle.classList.add('recette-title');
+        recetteTitle.textContent = 'RECETTE';
+
+        const description = document.createElement('p');
+        description.classList.add('recipe-description');
+        description.textContent = this.recipe.description;
+
+        const ingredientsTitle = document.createElement('h4');
+        ingredientsTitle.classList.add('ingredients-title');
+        ingredientsTitle.textContent = 'Ingrédients';
+
+        const ingredientsList = document.createElement('ul');
+        ingredientsList.classList.add('ingredients-list');
+        for (const ingredient of this.recipe.ingredients) {
+            const ingredientItem = document.createElement('li');
+            const ingredientName = document.createElement('div');
+            ingredientName.classList.add('ingredient-name');
+            ingredientName.textContent = ingredient.ingredient;
+
+            const ingredientQuantity = document.createElement('div');
+            ingredientQuantity.classList.add('ingredient-quantity');
+            ingredientQuantity.textContent = `${ingredient.quantity || ''} ${ingredient.unit || ''}`;
+
+            ingredientItem.appendChild(ingredientName);
+            ingredientItem.appendChild(ingredientQuantity);
+            ingredientsList.appendChild(ingredientItem);
+        }
+
+        cardContentContainer.appendChild(title);
+        cardContentContainer.appendChild(recetteTitle);
+        cardContentContainer.appendChild(description);
+        cardContentContainer.appendChild(ingredientsTitle);
+        cardContentContainer.appendChild(ingredientsList);
+
+        card.appendChild(imageContainer);
+        card.appendChild(cardContentContainer);
+
+        return card;
+    }
+}
